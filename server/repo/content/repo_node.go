4bc65d048fa55d885808794cd784b83734aa4c3c
franklin <franklin@weareinteractive.com>
2014-10-14 16:27:00 +0200

    fixed linkid

--- server/repo/content/repo_node.go
+++ server/repo/content/repo_node.go
@@ -8,7 +8,7 @@ import (
 type RepoNode struct {
 	Id            string                 `json:"id"`       // unique identifier - it is your responsibility, that they are unique
 	MimeType      string                 `json:"mimeType"` // well a mime type http://www.ietf.org/rfc/rfc2046.txt
-	LinkId        string                 `json:"linkIds"`  // (symbolic) link/alias to another node
+	LinkId        string                 `json:"linkId"`   // (symbolic) link/alias to another node
 	Groups        []string               `json:"groups"`   // which groups have access to the node, if empty everybody has access to it
 	URI           string                 `json:"URI"`
 	Name          string                 `json:"name"`
