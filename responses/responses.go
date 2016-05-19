a54924ee8d25910c12fa00d4b020c8d77767fb94
Frederik Löffert <frederik@zitrusmedia.de>
2016-05-19 17:15:44 +0200

    Fixes #5: added status 500 to each error reply + pumped version number

--- responses/responses.go
+++ responses/responses.go
@@ -4,17 +4,19 @@ import "fmt"
 
 // Error describes an error for humans and machines
 type Error struct {
+	Status  int    `json:"status"`
 	Code    int    `json:"code"`
 	Message string `json:"message"`
 }
 
 func (e Error) Error() string {
-	return fmt.Sprintf("code: %q, message: %q", e.Code, e.Message)
+	return fmt.Sprintf("status:%q, code: %q, message: %q", e.Status, e.Code, e.Message)
 }
 
 // NewError - a brand new error
 func NewError(code int, message string) *Error {
 	return &Error{
+		Status:  500,
 		Code:    code,
 		Message: message,
 	}
